3346be2b5e0f9aaeea8876bb4ba239992cb18d0c
Jose Antonio <joseantoniocabsan@gmail.com>
2022-11-27 13:55:03 +0100

    Cambios en la interfaz de Medicamentos y creada su implementacion

--- app/src/main/java/com/joseantonio/personalproject/proyectovighealth/interfaces/ConsultasMedicamento.java
+++ app/src/main/java/com/joseantonio/personalproject/proyectovighealth/interfaces/ConsultasMedicamento.java
@@ -4,14 +4,14 @@ import com.joseantonio.personalproject.proyectovighealth.objetos.Medicamento;
 
 import java.util.ArrayList;
 
-public interface consultasMedicamento {
+public interface ConsultasMedicamento {
 
     public long nuevoMedicamento(int idUsuario, String nombreMedicamento, int dosis,
                                     String medidaDosis, int periodicidad, String comentarios);
 
     public ArrayList<Medicamento> mostrarMedicamentos();
 
-    public boolean editarMedicamento(int id, int idUsuario, String nombreMedicamento, int dosis,
+    public boolean editarMedicamento(int id, String nombreMedicamento, int dosis,
                                   String medidaDosis, int periodicidad, String comentarios);
 
     public boolean eliminarMedicamento(int id);
